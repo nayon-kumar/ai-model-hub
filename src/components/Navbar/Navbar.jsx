3a1c5971aab256839b6cfe6891b4dca285dddf88
nayon-kumar <nayon.cse55@gmail.com>
2026-04-02 18:29:56 +0600

    Navbar designed

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,76 @@
 import React from "react";
+import logo from "../../assets/logo.png";
+import { ShoppingCart } from "lucide-react";
 
 const Navbar = () => {
   return (
     <div>
-      <p>This is navbar</p>
+      <div className="navbar bg-base-100 shadow-sm">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                {" "}
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />{" "}
+              </svg>
+            </div>
+            <ul
+              tabIndex="-1"
+              className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+            >
+              <li>
+                <a>Home</a>
+              </li>
+              <li>
+                <a>About</a>
+              </li>
+              <li>
+                <a>Services</a>
+              </li>
+              <li>
+                <a>Contact</a>
+              </li>
+            </ul>
+          </div>
+          <div className="flex items-center gap-3 cursor-pointer">
+            <img src={logo} className="h-10" alt="Logo" />
+            <a className="text-xl font-bold hidden sm:flex">Ai Hub</a>
+          </div>
+        </div>
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+              <a>Home</a>
+            </li>
+            <li>
+              <a>About</a>
+            </li>
+            <li>
+              <a>Services</a>
+            </li>
+            <li>
+              <a>Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div className="navbar-end flex items-center gap-3">
+          <div className="cursor-pointer px-3 py-3 bg-base-200 hover:bg-base-300 rounded-full">
+            <ShoppingCart />
+          </div>
+          <a className="btn btn-secondary rounded-full">Get Started</a>
+        </div>
+      </div>
     </div>
   );
 };
